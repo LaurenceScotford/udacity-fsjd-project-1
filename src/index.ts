c966fa7101047a88ef2c07be90aa1d4bbdd11c5d
Laurence Scotford <laurencescotford@gmail.com>
2021-07-21 21:02:48 +0100

    Add Express
    Add Express to project

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-const myFunc = (num: number): number => {
-  return num * num;
-};
+import express from 'express';
 
-export default myFunc;
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send("Server is running!");
+});
+
+app.listen(port);
+console.log(`Server is running on port ${port}`);
